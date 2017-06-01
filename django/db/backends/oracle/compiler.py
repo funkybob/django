839640fb6d494649c3e825c179f0dea68dc62338
Curtis Maloney <curtis@tinbrain.net>
2017-06-01 12:15:37 +1000

    Update compiler.py

--- django/db/backends/oracle/compiler.py
+++ django/db/backends/oracle/compiler.py
@@ -28,20 +28,15 @@ class SQLCompiler(compiler.SQLCompiler):
             # Wrap the base query in an outer SELECT * with boundaries on
             # the "_RN" column.  This is the canonical way to emulate LIMIT
             # and OFFSET on Oracle.
+
             high_where = ''
             if self.query.high_mark is not None:
                 high_where = 'WHERE ROWNUM <= %d' % (self.query.high_mark,)
-
-            if self.query.low_mark:
-                sql = (
-                    'SELECT * FROM (SELECT "_SUB".*, ROWNUM AS "_RN" FROM (%s) '
-                    '"_SUB" %s) WHERE "_RN" > %d' % (sql, high_where, self.query.low_mark)
-                )
-            else:
-                # Simplify the query to support subqueries if there's no offset.
-                sql = (
-                    'SELECT * FROM (SELECT "_SUB".* FROM (%s) "_SUB" %s)' % (sql, high_where)
-                )
+                if self.query.low_mark is not None:
+                    high_where += 'AND ROWNUM > %d' % (self.query.low_mark,)
+            sql = (
+                'SELECT * FROM (SELECT "_SUB".* FROM (%s) "_SUB" %s)' % (sql, high_where)
+            )
 
         return sql, params
 
