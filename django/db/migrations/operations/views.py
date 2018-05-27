36f5413f4b961872b2dd95ed4a8c51af0b5712be
Curtis Maloney <curtis@tinbrain.net>
2018-05-27 18:28:53 +1000

    Add DeleteView operation

--- django/db/migrations/operations/views.py
+++ django/db/migrations/operations/views.py
@@ -1,5 +1,5 @@
 
-from .models import CreateModel
+from .models import CreateModel, DeleteModel
 
 
 class CreateView(CreateModel):
@@ -15,3 +15,18 @@ class CreateView(CreateModel):
 
     def describe(self):
         return "Create view %s" % (self.name,)
+
+
+class DeleteView(DeleteModel):
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        model = to_state.apps.get_model(app_label, self.name)
+        if self.allow_migrate_model(schema_editor.connection.alias, model):
+            schema_editor.delete_view(model)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        model = from_state.apps.get_model(app_label, self.name)
+        if self.allow_migrate_model(schema_editor.connection.alias, model):
+            schema_editor.create_view(model)
+
+    def describe(self):
+        return "Delete view %s" % (self.name,)
